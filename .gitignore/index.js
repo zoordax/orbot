1266bea9f214971adf30f4d529f8c45d67a6b23a
zoordax <49092704+zoordax@users.noreply.github.com>
2019-03-30 20:52:32 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -15,7 +15,7 @@ bot.on('message', function (message) {
 		let Embed = new Discord.RichEmbed()
 			.setDescription("voici la liste des monstres la seul chose a faire pour avoir sa composition est de taper -> !le_nom_du_monstre")
 			.setColor("#211edc")
-			.addField(">>>Tous les monstres<<<", "\n Noceros \nfaucheuse\ndrider\nwyrm\nbete-des-neiges\ngriffon\nlarve\ngargantua\nepinator\ntroyen\nailes-noires\nSabrecroc\naile-de-givre\ntitan\nmorfalange\nreine-abeille \nchaman \ngolem")
+			.addField(">>>Tous les monstres<<<", "\n Noceros \nfaucheuse\ndrider\nwyrm\nbete-des-neiges\ngriffon\nlarve\ngargantua\nepinator\ntroyen\nailesnoires\nSabrecroc\nailedegivre\ntitan\nmorfalange\nreineabeille \nchaman \ngolem")
 			.setFooter("Trop bien ??")
 		message.channel.sendEmbed(Embed).then(console.log).catch(console.error)
 	}
@@ -57,7 +57,7 @@ bot.on('message', function (message) {
 		message.channel.send(">>><<<  \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
 	}
 
-	if (message.content === '!aile-noir') {
+	if (message.content === '!ailenoir') {
 		message.channel.send(">>><<<  \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
@@ -65,7 +65,7 @@ bot.on('message', function (message) {
 		message.channel.send(">>><<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
 	}
 
-	if (message.content === '!aile-de-givre') {
+	if (message.content === '!ailedegivre') {
 		message.channel.send(">>><<<  \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
@@ -77,7 +77,7 @@ bot.on('message', function (message) {
 		message.channel.send(">>><<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \nombre")
 	}
 
-	if (message.content === '!reine-abeille') {
+	if (message.content === '!reineabeille') {
 		message.channel.send(">>><<<\nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
