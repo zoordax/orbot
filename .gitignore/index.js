6c01ee79b97c942ddce2aed4368dfbd7c49b4205
zoordax <49092704+zoordax@users.noreply.github.com>
2019-03-30 21:02:32 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -26,67 +26,67 @@ bot.on('message', function (message) {
 	}
 
 	if (message.content === '!drider') {
-		message.channel.send(">>><<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
+		message.channel.send(">>>DRIDER<<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
 	if (message.content === '!wyrm') {
-		message.channel.send(">>><<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
+		message.channel.send(">>>WYRM<<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
 	}
 
 	if (message.content === '!betedesneiges') {
-		message.channel.send(">>><<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
+		message.channel.send(">>>BETES DES NEIGES<<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
 	if (message.content === '!griffon') {
-		message.channel.send(">>><<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
+		message.channel.send(">>>GRIFFON<<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
 	if (message.content === '!larve') {
-		message.channel.send(">>><<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
+		message.channel.send(">>>MEGA LARVE<<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
 	}
 
 	if (message.content === '!gargantua') {
-		message.channel.send(">>><<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
+		message.channel.send(">>>GARGANTUA<<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
 	}
 
 	if (message.content === '!epinator') {
-		message.channel.send(">>><<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
+		message.channel.send(">>>EPINATOR<<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
 	if (message.content === '!troyen') {
-		message.channel.send(">>><<<  \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
+		message.channel.send(">>>TROYEN<<<  \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
 	}
 
 	if (message.content === '!ailenoir') {
-		message.channel.send(">>><<<  \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
+		message.channel.send(">>>AILE NOIRE<<<  \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
 	if (message.content === '!sabrecroc') {
-		message.channel.send(">>><<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
+		message.channel.send(">>>SABRECROC<<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
 	}
 
 	if (message.content === '!ailedegivre') {
-		message.channel.send(">>><<<  \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
+		message.channel.send(">>>AILE DE GRIVE<<<  \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
 	if (message.content === '!titan') {
-		message.channel.send(">>><<< \nincinératrice \nElementalist \nprima \nsage tourmenté \ngobelin")
+		message.channel.send(">>>TITAN<<< \nincinératrice \nElementalist \nprima \nsage tourmenté \ngobelin")
 	}
 
 	if (message.content === '!morphalange') {
-		message.channel.send(">>><<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \nombre")
+		message.channel.send(">>>MORPHALANGE<<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \nombre")
 	}
 
 	if (message.content === '!reineabeille') {
-		message.channel.send(">>><<<\nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
+		message.channel.send(">>>REINE DES ABEILLES<<<\nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
 	if (message.content === '!chaman') {
-		message.channel.send(">>><<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
+		message.channel.send(">>>CHAMAN<<< \nChasseur de démon \nEclair pourpre \nRoublard \ntraqueuse \noiseau noir")
 	}
 
 	if (message.content === '!golem') {
-		message.channel.send(">>><<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
+		message.channel.send(">>>GOLEM<<< \nincinératrice \nElementalist \nprima \nreine des neiges \ngobelin")
 	}
 
 	if (message.content === '!Noceros') {
