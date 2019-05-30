3a071e2010c95fc6fbe9c24697b468f74405fd17
zoordax <49092704+zoordax@users.noreply.github.com>
2019-05-30 14:17:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,6 +21,10 @@ bot.on('message', function (message) {
 		message.channel.sendEmbed(Embed).then(console.log).catch(console.error)
 	}
 	
+	if (message.content === '!salut') {
+		message.channel.send("silence !")
+	}
+	
 	
 	if (message.content === '!ailedegivre') {
 		message.channel.send(">>>AILE DE GRIVE<<<\nOiseau Noir\nElementaliste\nIncinératrice\nOmbre\nchasseur de démon")
